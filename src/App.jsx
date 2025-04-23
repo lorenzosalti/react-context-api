5f914d30b21583bbea0252409d0ffafe7e661c42
lorenzosalti <lorenzosalti@gmail.com>
2025-04-23 16:16:13 +0200

    feat: add import and wrap by Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
+// react router import
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+// context import
+import PostsContext from "./contexts/PostsContext"
 
 // pages import
 import HomePage from "./pages/HomePage"
@@ -14,16 +18,18 @@ function App() {
 
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<DefaultLayout />}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/about-us" element={<AboutUs />} />
-          <Route path="/posts" element={<PostsPage />} />
-          <Route path="/posts/:id" element={<PostDetail />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <PostsContext.Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about-us" element={<AboutUs />} />
+            <Route path="/posts" element={<PostsPage />} />
+            <Route path="/posts/:id" element={<PostDetail />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </PostsContext.Provider>
   )
 }
 
